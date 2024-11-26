a95fa3536ef82f5e88205193dc31ed8987afc502
Pasha Sich <97337742+Kapidastra@users.noreply.github.com>
2024-11-26 15:10:34 +0100

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { meta: action } = require('eslint-plugin-node/lib/rules/no-sync');
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +9,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newArray = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      newState = { ...newState, ...action.extraData };
+      newArray.push({ ...newState });
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+      newArray.push({ ...newState });
+    }
+
+    if (action.type === 'clear') {
+      newState = {};
+      newArray.push({ ...newState });
+    }
+  }
+
+  return newArray;
 }
 
 module.exports = transformStateWithClones;
